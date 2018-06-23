18bea816caadb3e56bee9aa4b20b8efac487f545
honokani <honokani.kaoriga@gmail.com>
2018-06-24 04:52:43 +0900

    UPDATE readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# puyoteto  
+   *  I make puyopuyo and tetris!  
+      ... But I don't know how to Tick or 30fps-like something.  
+      Temporarily, I stop to make game here.  
+      I do [this tutorial](https://www.youtube.com/playlist?list=PL7C8fMD-89DKhlerIE3BrYNd0PlhA6Zch) in [this repository](http://github.com/honokani/tetris_in_elm).  
+
--- src/hello.elm
+++ src/hello.elm
@@ -1,8 +1,14 @@
-import Html            as H
-import Html                 exposing (Html,text,div,node)
-import Html.Attributes      exposing (class,attribute)
-import Array                exposing (Array)
 import Maybe
+import List            as L
+import Random          as Rnd
+import Array                  exposing (Array)
+
+import Html            as H
+import Html                   exposing (Html,text,div,node)
+import Html.Attributes        exposing (class,attribute)
+
+import Time
+import Keyboard        as Kbd
 
 -- stylesheet setting for elm-reactor
 stylesheet =
@@ -19,24 +25,43 @@ stylesheet =
 
 
 
+
+type Colors = Red
+            | Green
+            | Blue
+            | Purple
+
+showCol : Colors -> String
+showCol c = case c of
+    Red -> "red"
+    _   -> "black"
+
 type Cell a = Wall
             | Empty
             | FilledBy a
 
-type alias Block = Maybe String
+type alias Block = Cell (Maybe Colors)
 
-type alias Field = Array (Array (Cell Block))
+type alias Field = Array (Array Block)
+
 type alias VisibleField = Array (Array (Html Msg))
-
-type alias Model = { field : Field
-                   , next  : Array (Array Block)
-                   }
-
 
 
 -- main
-type Msg = SpendTime
-         | NoOp
+type alias Model = { gState  : GState
+                   , field   : Field
+                   , curr    : Field
+                   , next    : Field
+                   , time    : Time
+                   , rndSeed : Seed
+                   }
+
+type GState = Ready
+            | Playing
+
+type Msg = NoOp
+         | StartGame
+         | SpendTime
 
 main : Program Never Model Msg
 main =
@@ -47,14 +72,20 @@ main =
         , subscriptions = always Sub.none
         }
 
+
 -- init
 init : ( Model, Cmd Msg )
-init = ( { field = initField, next = initBlock }
+init = ( { gState = Ready
+         , field  = initField
+         , curr   = initBlock
+         , next   = initBlock
+         }
        , Cmd.none
        )
 
 hight = 16
 width = 8
+initField : Field
 initField = make2D width hight
             |> Array.indexedMap (\i ar ->
                     if i==0 || i==width-1 then
@@ -62,11 +93,20 @@ initField = make2D width hight
                     else
                         Array.set (hight-1) Wall ar
                )
-initBlock = Array.repeat 2 <| Array.repeat 2 (Just "red")
+initBlock : Field
+initBlock = Array.repeat 2 <| Array.repeat 2 <| FilledBy <| Just Red
+
 
 -- update
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model = (model, Cmd.none)
+update msg model = case msg of
+    StartGame ->
+        (model, Cmd.none)
+    SpendTime ->
+        (model, Cmd.none)
+    _         ->
+        (model, Cmd.none)
+
 
 -- view
 view : Model -> Html Msg
@@ -83,14 +123,45 @@ view ({ field, next }) =
 
 
 
-watchCell : Cell Block -> Html Msg
+
+
+
+
+tetrisBlocks : List (Array (Array Bool))
+tetrisBlocks = L.map (\bs -> Array.fromList <| List.map Array.fromList bs)
+               [ [ [False, False, False, False]
+                 , [False, True , True , False]
+                 , [False, True , False, False]
+                 , [False, True , False, False]
+                 ]
+               , [ [False, True , False, False]
+                 , [False, True , False, False]
+                 , [False, True , False, False]
+                 , [False, True , False, False]
+                 ]
+               ]
+
+encolorBlocks : Array (Array Bool) -> Field
+encolorBlocks bss = Array.map (\bs ->
+                        Array.map (\b -> case b of
+                            True -> FilledBy <| Just Red
+                            _    -> Empty
+                        ) bs
+                    ) bss
+
+
+watchCell : Block -> Html Msg
 watchCell cell = case cell of
     Wall        -> div [class "cell-wall"] [text "w"]
     FilledBy mc -> case mc of
-        Just col -> div [class "cell-filled"] [div [class ("block_"++col)] []]
+        Just col -> div [class "cell-filled"] [div [class ("block_" ++ showCol col)] []]
         _        -> div [class "cell-empty"] [text "e"]
     _          -> div [class "cell-empty"] [text "e"]
 
-make2D : Int -> Int -> Array (Array (Cell Block))
+make2D : Int -> Int -> Field
 make2D h w = Array.repeat h <| Array.repeat w Empty
+
+
+
+
 
